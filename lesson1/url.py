b176afa8b8a5dfc4d795674b77cb13d41c5697a9
Igor_Acer <i_gor88@mail.ru>
2021-05-02 22:37:53 +0700

    lesson4

--- lesson1/url.py
+++ lesson1/url.py
@@ -1,8 +1,16 @@
-from views import Index, Search, Contacts
+from views import Index, Search, Contacts, About, StudyPrograms, CoursesList, \
+    CreateCourse, CreateCategory, CategoryList, CopyCourse
 
 
 routes = {
     '/': Index(),
     '/search/': Search(),
-    '/contacts/': Contacts()
+    '/contacts/': Contacts(),
+    '/about/': About(),
+    '/study_programs/': StudyPrograms(),
+    '/courses-list/': CoursesList(),
+    '/create-course/': CreateCourse(),
+    '/create-category/': CreateCategory(),
+    '/category-list/': CategoryList(),
+    '/copy-course/': CopyCourse()
 }
